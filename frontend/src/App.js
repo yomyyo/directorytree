92d5320fad00260a4f9e7b4165fd152bc31a0ebc
Sammy Tang <webdeveloper@warmoth.com>
2024-12-03 14:44:03 -0800

    init project

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import DirectoryTree from "./components/DirectoryTree";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <DirectoryTree />
+    </div>
+  );
+}
+
+export default App;
